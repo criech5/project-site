02f543d0de13c0c4dc5c70eb9577e59ec58be83e
criech5 <criech5@gmail.com>
2021-11-27 01:34:56 -0700

    changed route element to correct one

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -7,7 +7,7 @@ import SignedIn from "./SignedIn";
 const Main = () => {
     return (
         <Routes>
-            <Route path='/' element={<Main />} />
+            <Route path='/' element={<Home />} />
             <Route path='/signedin' element={<SignedIn />} />
         </Routes>
     );
